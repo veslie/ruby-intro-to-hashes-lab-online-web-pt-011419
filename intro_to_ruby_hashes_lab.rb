44a0a8310c6dea95973ecb83703a51a37aa744fe
veslie losius <veslielosius@yahoo.com>
2019-02-15 05:57:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,37 +10,7 @@ end
 
 def monopoly
   monopoly =  {
-  :railroads=>
-  {
-    :pieces => ,
-    :rent_in_dollars =>
-    {
-      :one_piece_owned => " ",
-      :two_pieces_owned => " ",
-      :three_pieces_owned => " ",
-      :four_pieces_owned => " "
-    },
-    :names  =>
-    {
-      :reading_railroad =>
-      {
-        "mortgage_value"=>" "
-      },
-      :pennsylvania_railroad =>
-      {
-        "mortgage_value" ""
-      },
-      :b_and_o_railroad =>
-      {
-        "mortgage_value" ""
-      },
-      :shortline =>
-      {
-        "mortgage_value" ""
-        
-      }
-    }
-  }
-}
+  :
+      
 
     
