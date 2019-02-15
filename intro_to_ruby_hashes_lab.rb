962b5054b81856d8ef62ddc81d1845485ede0024
veslie losius <veslielosius@yahoo.com>
2019-02-15 06:45:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ def monopoly_with_third_tier
     }
   }
 end
-  def monopoly_with_third_tier
+  def monopoly_with_fourth_tier
   monopoly = {
     railroads: {
       pieces: 4, 
@@ -72,7 +72,7 @@ end
     }
   }
 end
-
+def monopoly_with_fourth_tier
   monopoly = {
     railroads: {
       pieces: 4, 
